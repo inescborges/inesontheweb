6dcec32fb5e5c0340e3650e1d4758ba3833a3c3e
ines.borges <iborges@alter-solutions.com>
2019-07-15 17:51:41 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -7,14 +7,6 @@
 
 $(function() {
     console.log( "ready!" );
-/*    $( ".menu-title" ).toggle(function() {
-        console.log("click");
-        var item = $(this).parent().siblings().children('.menu-list').children();
-        console.log(item);
-        $(item2).addClass('open');
-    }, function() {
-        $(item2).removeClass('open');
-    });*/
     $(".menu-title").click(function(){
         var item = $(this).parent().siblings().children('.menu-dropdown').children();
         console.log(item);
