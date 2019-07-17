0686a54645ece6069086908dba99f0d603d9e4f5
ines.borges <iborges@alter-solutions.com>
2019-07-17 17:58:55 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -5,11 +5,22 @@
     });
 });*/
 
-$(function() {
-    console.log( "ready!" );
-    $(".menu-title").click(function(){
-        var item = $(this).parent().siblings().children('.menu-dropdown').children();
-        console.log(item);
-        $(item).toggleClass("open");
-    });
+$(document).ready(function() {
+  $(".cont").addClass("animation");
+  // $(".list-title").addClass("list-title-hide");
+  $("img-container").hide();
+  $(".albums").find(".img-container").hide();
+  // function showList() {
+  //   $(".list-title").removeClass("list-title-hide");
+  // }
+  $(".list-title").click(function(){
+  if ($(this).closest(".albums").length>0) {
+    $(this).toggleClass("list-title-hide");
+    $(this).find(".menu-title").toggleClass("menu-title-show");
+    $(".albums").find(".img-container").delay(1000).toggle();
+  }
+  if ($("img-container").closest(".albums").length>0) {
+    $(this).delay(1000).show();
+  }
+});
 });
