7173497f661bfa9773d78a0e998b425679e62d02
inescborges <inescandeiasb@gmail.com>
2019-07-22 01:33:56 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
-/*$(document).ready(function(){
-    $('.menu-title').on('click', function () {
-        console.log("click");
-        $('.menu-dropdown > *').toggleClass('open-list');
-    });
-});*/
-
 $(document).ready(function() {
   $(".cont").addClass("animation");
   $(".img-container").addClass("img-hide");
-  // $(".list-title").addClass("list-title-hide");
-  // function showList() {
-  //   $(".list-title").removeClass("list-title-hide");
-  // }
   $(".list-title").click(function(){
-  if ($(this).closest(".list").length>0) {
+  if ($(this).closest(".list").length > 0) {
     $(this).closest(".list").toggleClass("list-show");
     $(this).toggleClass("list-title-hide");
     $(this).find(".menu-title").toggleClass("menu-title-hide");
-    // $(this).siblings(".img-container").toggleClass("img-show");
     if ($(this).siblings('.img-container').hasClass("img-hide")) {
       $(this).siblings(".img-container").addClass("img-show");
       $(this).siblings(".img-container").removeClass("img-hide");
